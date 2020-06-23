af4e2856e97dddaa2b53bd00ca5a6ae189f58a54
Conor Barnes <conorbarnes93@gmail.com>
2020-06-23 11:10:41 -0700

    Create tab functionality, shout-outs to w3schools

--- src/index.js
+++ src/index.js
@@ -4,6 +4,31 @@ import story from './story'
 import location from './location'
 import potatoes from './potatoes'
 
+function switchtab(event) {
+    let i, tabcontent, tabLinks, targetValue;
+
+    // reset all the others
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';
+    }
+
+    // Get all elements with class="tabLinks" and remove the class "active"
+    tabLinks = document.getElementsByClassName("tabLinks");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    console.log(event.path[0].value)
+
+    targetValue = event.path[0].value;
+
+    document.getElementById(targetValue).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+
 let tabs = [story, location, potatoes]
 
 const container = document.querySelector('#content');
@@ -14,11 +39,13 @@ container.appendChild(copy);
 const tabmenu = document.createElement('div');
 tabmenu.classList.add("tab")
 
+
 for (let i = 0; i < tabs.length; i++) {
     let button = document.createElement('button');
     button.classList.add('tabLinks');
     button.textContent = tabs[i].id;
     button.value = tabs[i].id;
+    button.addEventListener('click', switchtab)
     tabmenu.appendChild(button)
 }
 
@@ -32,4 +59,4 @@ for (let i = 0; i < tabs.length; i++) {
 
 
 
-console.log('success! success! success! success! success!')
+console.log('success! success! success! success! success! success!')
