7440e7d3735fdb3f690bf0b80b56a637d176f556
Conor Barnes <conorbarnes93@gmail.com>
2020-06-23 09:48:56 -0700

    create all the modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import { headline } from './pageload.js';
+import { headline, copy } from './pageload.js';
 import './style.css';
 
-document.body.appendChild(headline)
-
+const container = document.querySelector('#content')
+container.appendChild(headline)
+container.appendChild(copy)
 // test
 
-console.log('success! success! success! success!')
+console.log('success! success! success! success! success!')
--- src/pageload.js
+++ src/pageload.js
@@ -1,6 +1,10 @@
 const headline = document.createElement('h1')
 headline.textContent = "Potatoland Forever!"
 
+const copy = document.createElement('p');
+copy.textContent = "Potatoland was there for me when no one else was."
+
 export {
-    headline
+    headline,
+    copy
 }
